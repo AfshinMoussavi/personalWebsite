9aafc76db67f4c7a683f08cb6131f3e7481dde67
Afshin Moussavi <khodeafshin666@gmail.com>
2024-05-14 22:52:35 +0330

    .

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.blog_view  , name='index'),
-    path('single/', views.blog_single_view, name='single')
+    path('<int:pid>', views.blog_single_view, name='single')
 ]
